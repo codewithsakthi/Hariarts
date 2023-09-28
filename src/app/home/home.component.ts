d442bfaa46d29bdb8440d2e41880be7e96c7e230
codewithsakthi <developersakthi23@gmail.com>
2023-09-28 20:58:18 +0530

    new changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,11 +9,17 @@ import { Component } from '@angular/core';
 export class HomeComponent {
 
   images: string[] = [
-    '../../assets/back1.jpg',
-    '../../assets/back1.jpg',
-    '../../assets/back1.jpg',
-    '../../assets/back1.jpg',
-    '../../assets/back1.jpg',
+    '../../assets/Slider/1.jpg',
+    '../../assets/Slider/2.jpg',
+    '../../assets/Slider/3.jpg',
+    '../../assets/Slider/4.jpg',
+    '../../assets/Slider/5.jpg',
+    '../../assets/Slider/6.jpg',
+    '../../assets/Slider/7.jpg',
+    '../../assets/Slider/8.jpg',
+    '../../assets/Slider/9.jpg',
+
+
 
   ];
 
